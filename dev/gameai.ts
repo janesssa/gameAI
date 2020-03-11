b666c48c2101de29a3f0f3f5a2dd3a582ad3235c
janessa labeur <0935165@hr.nl>
2020-03-11 14:30:29 +0100

    [TASK] Added comments

--- dev/gameai.ts
+++ dev/gameai.ts
@@ -7,21 +7,25 @@ class GameAI {
     public static bestKnight: number
     
     public static minmax (depth: number, maximizingPlayer: boolean, gameState: GameState, king: King, knights: Knight[]) {
+        // If the game is over (win/lose) or if the depth is 0 return the score
         if(gameState.getScore()[1] || depth === 0){
             return gameState.getScore()[0]
         } else {
             if(maximizingPlayer){
                 let max = -Infinity
+                // Loop through all players to get the highest number 
                 knights.forEach((knight, player) => {
                     let legalMoves = knight.getMoves(gameState.knightPositions[player])
                     for(let i = 0; i < legalMoves.length; i++){
                         let newState = gameState.copy()
                         newState.knightPositions[player] = legalMoves[i]
                         let value = this.minmax(depth - 1, false, newState, king, knights)
+                        // When you are at the top level you need to save the move and player 
                         if(depth === this.DEPTH && value > max){
                             this.bestMove = newState.knightPositions[player]
                             this.bestKnight = player
                         }
+                        // Always update the max, even if your not in the top level
                         max = Math.max(max, value)
                     }
                 })
@@ -43,6 +47,8 @@ class GameAI {
 
     public static moveKnight(king:King, knights:Knight[], gameState:GameState) {
         let t0 = performance.now();
+
+        // Call the minimax function to start the AI
         this.minmax(this.DEPTH, true, gameState, king, knights)
 
         knights[this.bestKnight].setPosition(this.bestMove)
